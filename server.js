5d35e635c9d238b1bed92c34c54f3b52e0e79882
faheemfazal <faheemfazalpkv@gmail.com>
2023-05-10 15:13:17 +0530

    change render url

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ app.use(express.json())
 app.use(morgan('tiny'))
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://main.dgdcwln1z2552.amplifyapp.com'],
     methods: ["GET", "POST"],
     credentials: true,
 }))
 
-//   https://main.dgdcwln1z2552.amplifyapp.com
+//   https://main.dgdcwln1z2552.amplifyapp.com  http://localhost:3000
 
 app.use("/", routeruser);
 app.use("/admin", routerAdmin)
@@ -58,7 +58,7 @@ const server = app.listen(PORT, (err) => {
 // /socket io setting 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://main.dgdcwln1z2552.amplifyapp.com',
         // credentials:true
         methods: ['GET', 'POST']
     }
